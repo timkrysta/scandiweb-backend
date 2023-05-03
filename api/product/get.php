68ebeeaac4918f8d5ba9c5edca54e583b9942c84
donvoldemort <krystatymoteusz@gmail.com>
2023-05-03 13:20:25 +0200

    Made API endpoint to get all products or a specific product data

--- api/product/get.php
+++ api/product/get.php
@@ -4,50 +4,13 @@ ini_set('display_errors', 1);
 require $_SERVER['DOCUMENT_ROOT'] . '/web-developer-test-assignment/' .'vendor/autoload.php';
 
 use Timkrysta\Api;
-use Timkrysta\DB;
-use Timkrysta\Validator;
+use Timkrysta\Models\Product;
 use Timkrysta\Response;
 
 Api::exitIfHttpMethodNotIn(['GET']);
 
-
-
-$db = new DB();
-$data = [];
-
-if (isset($_GET['sku'])) {
-    // return specific product
-    $validationRules = [
-        'sku' => ['required', 'string', 'between:1,255', 'alpha_dash', 'exists:products,sku'],
-    ];
-    $validator = new Validator($_GET, $validationRules);
-
-    if ($validator->fails()) {
-        Response::json([
-            'message' => 'Validation Failed',
-            'error' => $validator->errors,
-        ], 400);
-    }
-
-    $query = "SELECT * FROM products WHERE sku = ?;";
-    $paramType = 's';
-    $paramArray = [
-        $_GET['sku']
-    ];
-    $result = $db->select($query, $paramType, $paramArray);
-    if ($result === null) {
-        Response::json(['message' => 'Getting the product failed'], 400);
-    }
-    $data = $result;
-} 
-else {
-    $query = "SELECT * FROM products;";
-    $result = $db->select($query);
-    if ($result === null) {
-        Response::json(['message' => 'Getting products failed'], 400);
-    }
-    $data = $result;
-}
-
+$data = isset($_GET['sku'])
+    ? Product::findBySku($_GET['sku'])
+    : Product::all();
 
 Response::json($data, 200);
