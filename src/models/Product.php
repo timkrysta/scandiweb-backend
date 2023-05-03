7278fb7a2d51125d2a86c760af7135e02da97031
donvoldemort <krystatymoteusz@gmail.com>
2023-05-03 13:19:47 +0200

    Created Timkrysta\Models\Product

---
+++ src/models/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Timkrysta\Models;
+
+use Timkrysta\DB;
+use Timkrysta\Validator;
+use Timkrysta\Response;
+
+class Product {
+    /* 
+     * Get specific product
+     */
+    public static function findBySku($sku): array
+    {
+        $validator = new Validator(['sku' => $sku], validationRules: [
+            'sku' => ['required', 'string', 'between:1,255', 'alpha_dash', 'exists:products,sku'],
+        ]);
+
+        if ($validator->fails()) {
+            Response::json([
+                'message' => 'Validation Failed',
+                'error' => $validator->errors,
+            ], 400);
+        }
+
+        $db = new DB();
+        $result = $db->select(
+            "SELECT * FROM products WHERE sku = ?;", 
+            's', 
+            [ $_GET['sku'] ]
+        );
+        if ($result === null) {
+            Response::json(['message' => 'Getting the product failed'], 400);
+        }
+        return $result[0];
+    }
+
+    /* 
+     * Get all products
+     */
+    public static function all(): array
+    {
+        $db = new DB();
+        $result = $db->select("SELECT * FROM products;");
+        if ($result === null) {
+            Response::json(['message' => 'Getting products failed'], 400);
+        }
+        return $result;
+    }
+}
