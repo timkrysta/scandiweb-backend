7105c6249951f51df18ccb3bf0b931c291787c05
donvoldemort <krystatymoteusz@gmail.com>
2023-05-03 22:45:22 +0200

    Allowed CORS Requests to API

--- src/Response.php
+++ src/Response.php
@@ -5,9 +5,17 @@ namespace Timkrysta;
 class Response {
     public static function json(mixed $data, int $response_code = 200): void
     {
+        self::allowCorsRequests();
         header('Content-Type: application/json');
         http_response_code($response_code);
         echo json_encode($data);
         exit();
+    }
+    public static function allowCorsRequests()
+    {
+        // Allow cross-origin requests
+        header("Access-Control-Allow-Origin: http://localhost:3000");
+        #header("Access-Control-Allow-Methods: GET, POST");
+        #header("Access-Control-Allow-Headers: Content-Type, Authorization");
     }
 }
